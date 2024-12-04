aa92912e9593cf7bd0307db096ad85778b15cdb5
amrcs92 <amrcs1992@gmail.com>
2024-12-04 13:54:45 +0200

    edit ClubsSection (ar & en fix redirect to cart)

--- src/components/ar/ClubsSection.vue
+++ src/components/ar/ClubsSection.vue
@@ -638,17 +638,14 @@ export default {
           totalCartItems = response.data.data.total_items_count;
           const textElement1 = document.getElementById("totalCount1");
           textElement1.textContent = totalCartItems > 0 ? totalCartItems : 0;
-          console.log('added item');
+          
           this.fetchCartItems();
-          console.log('before redirect');
           
           this.$router.push({ path: "/ar/cart/", name: "CartAr" })
           .then(() => {
             // Reload the page after the redirect
             location.reload();
           });
-
-          console.log('redirected');
         })
         .catch(error => {
           console.error(
@@ -674,8 +671,6 @@ export default {
           session_group_data: lastStudent.session_group_data
         };
 
-        console.log("Payload for POST request (after popup close):", payload);
-
         axios
           .post("/api/session/club-session-cart", payload, {
             headers: {
@@ -683,11 +678,6 @@ export default {
             }
           })
           .then(response => {
-            console.log(
-              "Cart updated successfully after popup close:",
-              response.data
-            );
-            
             this.fetchCartItems();
             // this.$router.push({ path: "/ar/cart/", name: "CartAr" });
           })
